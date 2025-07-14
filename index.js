01a72e60b18f3ec4fd686ba9eb267c87f1157f91
08prajwal2000 <manuaradhya07.uk@gmail.com>
2025-07-14 19:38:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ import { writeFileSync, mkdirSync, existsSync } from "fs";
 
 if (!existsSync("./build")) mkdirSync("./build")
 
-writeFileSync("./build/test.html", `
+writeFileSync("./build/index.html", `
   <html>
     <head>
       <title>Web App 001</title>
+      <link rel="stylesheet" href="/style.css">
     </head>
     <body>
       <h1>Hello this is a web app 001</h1>
     </body>
   </html>
 
+`, {
+  flag: "a"
+});
+writeFileSync("./build/style.css", `
+body {
+  background-color: green;
+  color: white;
+}
+h1 {
+  font-size: 2rem;
+  border: 2px solid red;
+}
 `, {
   flag: "a"
 });
